91fae900e3e698e753b612067432c5b1c83f980f
Isabella Castro <isabellacastro1125@gmail.com>
2024-10-28 15:33:01 -0400

    Updating config

---
+++ src/main/java/com/isabella/professionalportfolioapp/WebConfig.java
@@ -0,0 +1,17 @@
+package com.isabella.professionalportfolioapp;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:4200") // Change this to your Angular app URL
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
+    }
+}
